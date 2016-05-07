44b02fc5820b2a3c29537677c6ba77c838567bfa
Jenny Brown <jennybrown8@gmail.com>
2016-05-07 14:58:42 -0500

    Cleanup

--- src/test/java/com/codeforanyone/codergirl/userwebapp/Start.java
+++ src/test/java/com/codeforanyone/codergirl/userwebapp/Start.java
@@ -2,58 +2,25 @@ package com.codeforanyone.codergirl.userwebapp;
 
 import org.eclipse.jetty.security.HashLoginService;
 import org.eclipse.jetty.server.Server;
-//import org.eclipse.jetty.util.thread.ScheduledExecutorScheduler;
 import org.eclipse.jetty.webapp.WebAppContext;
 
 public class Start {
 	public static void main(String[] args) throws Exception {
 		// Create a basic jetty server object that will listen on port 8080.
-		// Note that if you set this to port 0 then a randomly available port
-		// will be assigned that you can either look in the logs for the port,
-		// or programmatically obtain it for use in test cases.
 		Server server = new Server(8080);
 
-		//server.addBean(new ScheduledExecutorScheduler());
-
+		// Tell it where to find the code and jsps relative to our project.
 		WebAppContext bb = new WebAppContext();
 		bb.setServer(server);
 		bb.setContextPath("/");
 		bb.setWar("src/main/webapp");
 		server.setHandler(bb);
 
-		/*
-		 * String jetty_base =
-		 * "/Users/jennybrown/Documents/workspace/userwebapp/target"; String
-		 * jetty_war_dir =
-		 * "/Users/jennybrown/Documents/workspace/userwebapp/target";
-		 * 
-		 * File f_jetty_base = new File(jetty_war_dir); if
-		 * (!f_jetty_base.isDirectory()) { boolean success =
-		 * f_jetty_base.mkdirs(); } System.setProperty("jetty.base",
-		 * jetty_base);
-		 * 
-		 * // Handler Structure HandlerCollection handlers = new
-		 * HandlerCollection(); ContextHandlerCollection contexts = new
-		 * ContextHandlerCollection(); handlers.setHandlers(new Handler[] {
-		 * contexts, new DefaultHandler() }); server.setHandler(handlers);
-		 * 
-		 * DeploymentManager deployer = new DeploymentManager();
-		 * deployer.setContexts(contexts); deployer.setContextAttribute(
-		 * "org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern", ".*" +
-		 * "/servlet-api-[^/]*\\.jar$");
-		 * 
-		 * WebAppProvider webapp_provider = new WebAppProvider();
-		 * webapp_provider.setMonitoredDirName(jetty_war_dir);
-		 * webapp_provider.setScanInterval(1);
-		 * webapp_provider.setExtractWars(true);
-		 * webapp_provider.setConfigurationManager(new
-		 * PropertiesConfigurationManager());
-		 * 
-		 * deployer.addAppProvider(webapp_provider); server.addBean(deployer);
-		 */
-
+		// Turn down logging so it's less noisy.
 		server.setDumpAfterStart(false);
 		server.setDumpBeforeStop(false);
+		
+		// Stop jetty when we exit Eclipse.
 		server.setStopAtShutdown(true);
 
 		// Configure a LoginService.
@@ -70,10 +37,10 @@ public class Start {
 		server.addBean(loginService);
 
 		try {
-			System.out.println(">>> STARTING EMBEDDED JETTY SERVER, PRESS ANY KEY TO STOP");
+			System.out.println(">>> STARTING EMBEDDED JETTY SERVER. Click this window and press any key to stop.");
 			server.start();
 			System.in.read();
-			System.out.println(">>> STOPPING EMBEDDED JETTY SERVER");
+			System.out.println(">>> STOPPING EMBEDDED JETTY SERVER.");
 			server.stop();
 
 			// The use of server.join() the will make the current thread join
@@ -81,6 +48,7 @@ public class Start {
 			// http://docs.oracle.com/javase/7/docs/api/java/lang/Thread.html#join()
 			server.join();
 		} catch (Exception e) {
+			// basic error reporting to console.
 			e.printStackTrace();
 			System.exit(1);
 		}
