9ab406046f125f080b798338716d19c044eb81f6
Jenny Brown <jennybrown8@gmail.com>
2016-05-07 19:09:02 -0500

    Demo of a servlet request parameter

--- src/main/java/com/codeforanyone/edujavajsp/HelloServlet.java
+++ src/main/java/com/codeforanyone/edujavajsp/HelloServlet.java
@@ -8,18 +8,33 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.eclipse.jetty.util.log.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class HelloServlet extends HttpServlet {
+    static Logger log = LoggerFactory.getLogger(HelloServlet.class);
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		log.debug("Received a doGet() request");
+		resp.setContentType("text/html"); // What does it do if you set "text/plain" ? Try it!
+		
 		PrintWriter pw = resp.getWriter();
-		pw.println("Hello!");
-		resp.flushBuffer();
+		String firstname = "";
+		if (req.getParameter("firstname") != null) {
+			firstname = req.getParameter("firstname");
+	 		pw.println("<h1>Hello, " + firstname + "!</h1>");
+	 	} else {
+	 		pw.println("<h1>Hello!</h1>");
+	 	}
+		
+		pw.close();
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		log.debug("Received a doPost() request");
 		super.doPost(req, resp);
 	}
 
